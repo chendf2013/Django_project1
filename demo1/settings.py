39b3060a578c6a3ba61f4e3f3cceba5a979875ce
陈东发 <chendf2013@163.com>
2018-09-14 23:45:08 -0700

    修改静态文件路径

--- demo1/settings.py
+++ demo1/settings.py
@@ -123,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+# 相对路径，其他的app的都能使用
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static_files'),
+]
+
