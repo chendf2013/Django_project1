cc5a4e59444fa0cd6eeee68c63d8cc7e8f5944de
陈东发 <chendf2013@163.com>
2018-09-19 05:12:49 -0700

    自定义模板表单

--- booktest/forms.py
+++ booktest/forms.py
@@ -1,6 +1,18 @@
 from django import forms
 
+# class BookForm(forms.Form):
+#     """普通的表单"""
+#     title = forms.CharField(label="书名",required=True,max_length=50)
+#     pub_date = forms.DateField(label="出版日期",required=True)
+#
+from booktest.models import BookInfo
 
-class BookForm(forms.Form):
-    title = forms.CharField(label="书名",required=True,max_length=50)
-    pub_date = forms.DateField(label="出版日期",required=True)
+
+class BookForm(forms.ModelForm):
+    """模板表单"""
+
+    class Meta:
+        # 制定模板
+        model = BookInfo
+        # 向表单内添加模板内的字段
+        fields = ["btitle", "bcomment", "bpub_date"]
